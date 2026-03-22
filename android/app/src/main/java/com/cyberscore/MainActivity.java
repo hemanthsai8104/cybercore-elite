1d61f352b7402094fd076d8648fafcfa0757121e
Hemanth Sai <donavallihemanthsai@gmail.com>
2026-03-23 03:01:19 +0530

    feat(apk): switch to professional manual android webview project build

---
+++ android/app/src/main/java/com/cyberscore/MainActivity.java
@@ -0,0 +1,22 @@
+package com.cyberscore;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView myWebView = new WebView(this);
+        setContentView(myWebView);
+        
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://cybercore-elite.vercel.app");
+    }
+}
