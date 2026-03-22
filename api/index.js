3bb04abe4a04e934da2cdb3442f1df5fe836ae97
Hemanth Sai <donavallihemanthsai@gmail.com>
2026-03-22 18:59:18 +0530

    Anchored 100% visual sight for cloud landing page

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 // --- DYNAMIC INTELLIGENCE ENGINE ---
